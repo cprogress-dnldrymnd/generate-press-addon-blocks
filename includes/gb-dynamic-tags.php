0c39774d879c3bafcf7ee718144ed14376b53043
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-05 21:04:08 +0800

    Update gb-dynamic-tags.php

--- includes/gb-dynamic-tags.php
+++ includes/gb-dynamic-tags.php
@@ -25,7 +25,7 @@ function dd_register_gb_dynamic_tags() {
     new GenerateBlocks_Register_Dynamic_Tag( [
         'title'    => __( 'Archive/Post Title', 'dd-gp-addon-blocks' ), // The human-readable name shown in the selector UI.
         'tag'      => 'dd_archive_post_title',                          // The internal tag identifier.
-        'type'     => 'site',                                           // Groups the tag in the interface.
+        'type'     => 'elements',                                           // Groups the tag in the interface.
         'return'   => 'dd_gb_dynamic_archive_post_title_callback',      // The PHP callback that generates the output.
     ] );
 }
